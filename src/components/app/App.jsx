6b25d25ebed23e02bc870ceecd13d7fd6b0255cd
biddut mia <test@gamil.com>
2023-10-17 07:41:57 -0700

    two route created

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,30 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
+const Home = () => {
+  return <div>this is a home page</div>;
+};
+
+const Other = () => {
+  return <div>this is a other page</div>;
+};
+
 const App = () => {
   return (
     <div style={{ width: "80%", margin: "auto" }}>
-      <div>
-        hello world Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur voluptatibus a rem nihil at sapiente labore architecto in,
-        quos sunt vero illo amet repellendus, quaerat ex! Sit omnis dolore vitae
-        quia sint dolor laudantium aspernatur similique hic ipsa commodi quidem,
-        dolores error provident iusto cupiditate, perspiciatis tempora aut nemo!
-        Quidem nobis illum repellat quibusdam corrupti rerum optio unde, at ipsa
-        placeat cumque ex omnis eum quisquam officiis nostrum ea fuga
-        reiciendis, vero architecto non adipisci. Tempora adipisci consequatur
-        exercitationem recusandae beatae vitae dolorum quidem doloribus ratione
-        quos laudantium quasi sapiente natus autem quia dolor commodi,
-        distinctio laborum cupiditate. Numquam, iure qui! Reiciendis repellat,
-        voluptate corporis perspiciatis nemo id doloribus autem, placeat fugit
-        eveniet obcaecati iure accusamus iusto quo corrupti quae voluptatem
-        excepturi nulla laborum, aliquam natus ut sed non. Numquam culpa officia
-        aspernatur dignissimos sed explicabo, temporibus quod minus alias
-        tempora quisquam fugit praesentium earum animi beatae magni sequi
-        repudiandae, vitae provident molestias qui vel? Officia commodi
-        inventore, distinctio quas minus reiciendis qui delectus illum
-        consequuntur earum aperiam a dolorum sequi temporibus dolore voluptatum
-        animi aliquid error in? Laudantium, quaerat sunt. Dolorem sed
-        dignissimos ratione iure sequi, vel maiores inventore, commodi alias
-        praesentium veniam vero. Exercitationem alias eius earum esse.
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/other" element={<Other />}></Route>
+      </Routes>
     </div>
   );
 };
