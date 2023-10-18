18d1ce8335f9ef6973278fc78c3ad976aa2af917
biddut mia <test@gamil.com>
2023-10-18 02:34:52 -0700

    data fetch by axios

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,37 +1,23 @@
 import { useStoreState } from "easy-peasy";
+import { useStoreActions } from "easy-peasy";
 import { Routes, Route } from "react-router-dom";
 
 const Home = () => {
   const data = useStoreState((state) => state.playLists.data);
-  console.log("data", data);
+
+  const actions = useStoreActions((action) => action.playLists.getPlayListItem);
 
   return (
     <div>
+      <button onClick={() => actions("PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl")}>
+        add playlist
+      </button>
       <div>this is a home page </div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, sed!
-        Minus nemo tenetur rem recusandae. Perspiciatis sunt modi, dicta quia
-        sit corporis eius molestias veniam, sequi, suscipit ad. Impedit
-        assumenda omnis dicta quos rem vel, deleniti repellendus et ratione nemo
-        molestiae ad ducimus, nostrum, quae eos earum similique? Facere non
-        repellendus corrupti inventore eius, quae possimus tempore expedita
-        nihil fuga placeat ipsa magni a tenetur blanditiis harum, ratione
-        voluptas maxime, officiis natus! Id accusamus exercitationem natus quod,
-        voluptatibus provident dignissimos dolorum repellendus dicta quasi ea
-        debitis aspernatur perspiciatis minima. Sit, accusantium neque vitae
-        amet non voluptatibus adipisci aliquam molestiae soluta odio, facilis
-        dolore! Quo cumque commodi culpa sunt excepturi omnis praesentium,
-        possimus aliquam aliquid corporis perferendis molestiae maxime, vero
-        laborum molestias vitae distinctio facilis aspernatur similique
-        necessitatibus! Deleniti ipsa autem vel, incidunt cupiditate magni.
-        Pariatur architecto quas inventore amet corrupti consequatur quam nobis
-        quos numquam vel mollitia sed error ex, unde voluptate deserunt odit
-        impedit nulla asperiores tempora, maxime sunt provident delectus
-        praesentium. Fugiat cum corporis quae similique laboriosam amet
-        distinctio aspernatur quidem dolorum, in eius provident eveniet sapiente
-        voluptatum eligendi alias nobis? A quo provident culpa hic rem iste ut
-        accusamus minus, minima magni odio sunt ducimus obcaecati similique.
-      </p>
+      {Object.values(data).length > -1 &&
+        Object.values(data).map((v, i) => {
+          console.log(v);
+          return <div key={i}>{v.playListDescription}</div>;
+        })}
     </div>
   );
 };
